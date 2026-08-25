57b5071862d7f8683553c3a207ef23064286c349
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-08-25 11:34:59 +0530

    [Shreyash] Add. Create a Create a standardized Exchange  dimension that is used for answering like Asset classes by exchange, Instruments by exchange

---
+++ sql/dimensions/03_dim_exchange.sql
@@ -0,0 +1,11 @@
+
+IF OBJECT_ID('dbo.DimExchange') IS NOT NULL DROP TABLE dbo.DimExchange;
+CREATE TABLE dbo.DimExchange (
+    exchange_key    INT IDENTITY(1,1) PRIMARY KEY,
+    exchange_name   VARCHAR(150) NOT NULL,
+    exchange_code   VARCHAR(10)  NOT NULL,
+    country_key     INT          NULL REFERENCES dbo.DimCountry(country_key),
+    region          VARCHAR(100) NULL,
+    market_type     VARCHAR(50)  NULL,
+    CONSTRAINT UQ_DimExchange_Code UNIQUE (exchange_code)
+);
